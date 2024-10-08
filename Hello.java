26db95b288dc38ea5977e4adfa15a9cd09ab6739
Nijen Patel <nijenbhut@gmail.com>
2024-10-08 19:44:16 +0530

    User input field added

--- Hello.java
+++ Hello.java
@@ -1,9 +1,15 @@
+
+import java.util.Scanner;
+
 public class Hello {
 
     public static void main(String[] args) {
         
         System.out.println("Hello World");
         System.out.println("First code in java");
-        
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("You entered: " + number);
     }
 }
